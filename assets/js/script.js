182595c77f15688a924331fedf898259ac25cb49
ori27_ops <pironaoriana@gmail.com>
2024-10-03 12:05:45 -0500

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,6 @@
+//Impotamos la app
+import { app } from "./firebase.js";
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import "./signupForm.js"
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDcnF9dcWdh4lvSoIy943GcbKhP91mdJuk",
-    authDomain: "diario-digital-471e7.firebaseapp.com",
-    projectId: "diario-digital-471e7",
-    storageBucket: "diario-digital-471e7.appspot.com",
-    messagingSenderId: "265187515709",
-    appId: "1:265187515709:web:cb4cf1932f40b06f63f954"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+console.log(app);
---
+++ assets/js/signupForm.js
@@ -0,0 +1,33 @@
+import {auth} from "./firebase.js"
+import {createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js"
+import { showMessage } from "./toastMessage.js";
+
+const signupForm = document.querySelector("#signup-form")
+
+signupForm.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+    console.log("formulario enviado");
+
+    const email = signupForm["signup-email"].value;
+    const password = signupForm["signup-password"].value;
+
+    try {
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password); 
+        
+        
+        console.log(userCredentials); 
+
+        showMessage("Usuario registrado", "success");
+    } catch(error){
+        console.log(error);
+        if (error.code === "auth/email-already-in-use"){
+            showMessage("El correo ya está en uso", "error");
+        } else if (error.code === "auth/weak-password") {
+            showMessage("La contraseña debe tener al menos 6 caracteres ");
+        } else {
+            showMessage("Correo invalido", "error");
+        }
+    }
+});
+ 
