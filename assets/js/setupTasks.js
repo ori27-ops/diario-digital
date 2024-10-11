ee0048e8c49708be16ff93470d59402f6af8c674
ori27_ops <pironaoriana@gmail.com>
2024-10-10 21:37:49 -0500

    cambio

--- assets/js/setupTasks.js
+++ assets/js/setupTasks.js
@@ -22,7 +22,7 @@ export const setupTasks = (user) => {
         try {
             if (!editStatus) {
               // Crear tarea
-              await createTask(title, description, user.displayName, user.photoURL);
+              await createTask(title, description, user.displayName, user.photoURL, user.email);
               // Mostrar mensaje de éxito
               showMessage("Tarea creada", "success");
               // Limpiar el formulario
@@ -60,13 +60,19 @@ export const setupTasks = (user) => {
       <article class="task-container border border-2 rounded-2 p-3 my-3">
         <header class="d-flex justify-content-between align-items-center">
           <div class="d-flex align-items-center gap-3">
-            <img class="task-profile-picture rounded-circle" src="${data.userImage}" alt="${data.userName}" />
+            <img class="task-profile-picture rounded-circle" src="${
+              data.userImage
+            }" alt="${data.userName}" />
             <p class="m-0">${data.userName}</p>
           </div>
-          <div>
+          ${
+            user.email === data.userEmail
+              ? `<div>
             <button class="btn btn-info btn-editar" data-id="${doc.id}"><i class="bi bi-pencil-fill"></i> Editar</button>
             <button class="btn btn-danger btn-eliminar" data-id="${doc.id}"><i class="bi bi-trash3-fill"></i> Eliminar</button>
-          </div>
+          </div>`
+              : `<div></div>`
+          }
         </header>
         <hr />
         <h4>${data.title}</h4>
